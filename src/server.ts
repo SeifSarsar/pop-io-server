14c359709c39f13f9c76b00df2bc1e42dca7a99f
Seif Sarsar <seifsarsar@hotmail.com>
2021-09-04 23:23:08 -0400

    fix client url

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const server = createServer();
 server.listen(port);
 console.log(`Server listening on port ${port}`);
 
-const clientURL = 'https://spacepop.io';
+const clientURL = 'https://www.spacepop.io';
 //const clientURL = 'https://pop-io-client.herokuapp.com';
 //const clientURL = 'http://localhost:8080';
 const io = new Server(server, {
